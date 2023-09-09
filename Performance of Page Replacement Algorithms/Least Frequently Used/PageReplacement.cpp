ba064a9b26a1ea19641537497e6bf73cf7ae120e
jelhamm <elhamjafari200312@gmail.com>
2023-09-09 16:26:17 +0430

    Added PageReplacement class for implementing page replacement algorithms.

---
+++ Performance of Page Replacement Algorithms/Least Frequently Used/PageReplacement.cpp
@@ -0,0 +1,13 @@
+/* This function implements the page replacement algorithm
+ * and keeps track of the number of page faults.
+ * Short comment explaining the code's functionality.
+ */
+
+
+#include "PageReplacement.h"
+
+PageReplacement::PageReplacement(int capacity) : capacity(capacity), pageFaults(0) {}
+
+int PageReplacement::getPageFaults() const {
+    return pageFaults;
+}
